6264dff63bc9853c59621ab11abf1ecdf86f5764
Khoa <khoaho120703@gmail.com>
2025-05-23 21:46:18 +0700

    [Modify]_Complete Create Course API

--- app/api/generate-course-layout/route.jsx
+++ app/api/generate-course-layout/route.jsx
@@ -2,6 +2,7 @@ import { db } from "@/config/db";
 import { coursesTable } from "@/config/schema";
 import { currentUser } from "@clerk/nextjs/server";
 import { GoogleGenAI } from "@google/genai";
+import axios from "axios";
 import { NextResponse } from "next/server";
 const PROMPT = `Genrate Learning Course depends on following details. In which Make sure to add Course Name, Description, Course Banner Image Prompt (Create a modern, flat-style 2D digital illustration representing user Topic. Include UI/UX elements such as mockup screens, text blocks, icons, buttons, and creative workspace tools. Add symbolic elements related to user Course, like sticky notes, design components, and visual aids. Use a vibrant color palette (blues, purples, oranges) with a clean, professional look. The illustration should feel creative, tech-savvy, and educational, ideal for visualizing concepts in user Course) for Course Banner in 3d format Chapter Name,, Topic under each chapters, Duration for each chapters etc, in JSON format only
 
@@ -60,7 +61,9 @@ export async function POST(req) {
   const RawResp = response?.candidates[0]?.content?.parts[0]?.text;
   const RawJson = RawResp.replace("```json", "").replace("```", "");
   const JSONResp = JSON.parse(RawJson);
-
+  const ImagePrompt = JSON.course?.bannerImagePrompt;
+  //Generate Image
+  const bannerImageUrl = await GenerateImage(ImagePrompt);
   //Save to Database
 
   const result = await db.insert(coursesTable).values({
@@ -68,6 +71,29 @@ export async function POST(req) {
     courseJson: JSONResp,
     userEmail: user?.primaryEmailAddress?.emailAddress,
     cid: courseId,
+    bannerImageUrl: bannerImageUrl,
   });
   return NextResponse.json({ courseId: courseId }, { status: 201 });
 }
+
+const GenerateImage = async (imagePrompt) => {
+  const BASE_URL = "https://aigurulab.tech";
+  const result = await axios.post(
+    BASE_URL + "/api/generate-image",
+    {
+      width: 1024,
+      height: 1024,
+      input: imagePrompt,
+      model: "sdxl", //'flux'
+      aspectRatio: "16:9", //Applicable to Flux model only
+    },
+    {
+      headers: {
+        "x-api-key": process.env.AI_GURU_LAB_API, // Your API Key
+        "Content-Type": "application/json", // Content Type
+      },
+    }
+  );
+  console.log(result.data.image); //Output Result: Base 64 Image
+  return result.data.image;
+};
